e75649a52721ed338c142dbb810259182b742efd
root <root>
2025-11-27 20:38:52 +0530

    fix fstab

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -118,6 +118,12 @@ if $BUILD_ROM; then
     "$SRC_DIR/scripts/internal/create_work_dir.sh" || exit 1
     LOG_STEP_OUT
 
+    if [ -d "$SRC_DIR/target/$TARGET_CODENAME/patches" ]; then
+        LOG_STEP_IN true "Applying device patches"
+        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/target/$TARGET_CODENAME/patches" || exit 1
+        LOG_STEP_OUT
+    fi
+
     if [ -d "$SRC_DIR/unica/patches" ]; then
         LOG_STEP_IN true "Applying ROM patches"
         "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/unica/patches" || exit 1
@@ -130,11 +136,6 @@ if $BUILD_ROM; then
         LOG_STEP_OUT
     fi
 
-    if [ -d "$SRC_DIR/target/$TARGET_CODENAME/patches" ]; then
-        LOG_STEP_IN true "Applying device patches"
-        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/target/$TARGET_CODENAME/patches" || exit 1
-        LOG_STEP_OUT
-    fi
 
     if [ -d "$APKTOOL_DIR" ]; then
         LOG_STEP_IN true "Building APKs/JARs"
