3fb3587262955773428e7a775be8263ca07586c3
root <root>
2025-12-02 12:01:02 +0530

    .

--- target/r9q2/patches/camera/customize.sh
+++ target/r9q2/patches/camera/customize.sh
@@ -1,4 +1,2 @@
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
-
 
