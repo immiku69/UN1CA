79a609633c3568a1033e6ee9895bc341b31d6654
root <root>
2025-12-05 10:05:32 +0530

    .

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -3,3 +3,5 @@ SMALI_PATCH "system" "system/framework/services.jar" \
     "smali/com/android/server/RescueParty.smali" "return" \
     '-$$Nest$smisDisabled()Z' \
     'true'
+
+SET_PROP_IF_DIFF "vendor" "ro.oem_unlock_supported" "0"
